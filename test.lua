f84cbb0c8f9f0629bfff6ebef985852ddc75fc33
BoBThegrEat297 <50116108+BoBThegrEat297@users.noreply.github.com>
2023-12-28 10:54:44 +0000

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,5 @@
+-- This is a test script hosted on GitHub.
+-- When fetched and loaded from Roblox Studio, it will print a confirmation message.
+
+print("The script from GitHub has been loaded successfully!")
+print("Hello from the GitHub Lua script!")
